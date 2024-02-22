3b513f7d75bd7760a1a12b99b7f2de685487f134
Nicomicu <112415563+Nicomicu@users.noreply.github.com>
2024-02-22 10:03:50 -0300

    proyecto-finalizado

--- frontend/src/app/(privatePart)/listTask/components/ListTaskColumn.jsx
+++ frontend/src/app/(privatePart)/listTask/components/ListTaskColumn.jsx
@@ -1,4 +1,5 @@
 "use client"
+
 import useTask from "../hook/useTask"
 import Header from "./Header"
 import ColumnId from "./ColumnId"
@@ -11,20 +12,17 @@ const ListTaskColumnPage = () => {
   return (
     <>
       <Header />
-      <div>
-        <div className="flex flex-col">
-          <div className="mt-44 mx-auto w-[100rem] text-gray-600 font-bold text-2xl px-10 rounded-lg flex-nowrap">
-            <div className="flex justify-between">
-              {columns.map((columnId) => (
-                <ColumnId
-                  key={columnId}
-                  columnId={columnId}
-                  tareas={tareas}
-                  columns={columns}
-                />
-              ))}
-            </div>
-          </div>
+
+      <div className="grid grid-cols-3 ml-[17rem]">
+        <div className="mt-44 w-[100rem] flex justify-between text-gray-600 font-bold text-2xl px-10 rounded-lg ">
+          {columns.map((columnId) => (
+            <ColumnId
+              key={columnId}
+              columnId={columnId}
+              tareas={tareas}
+              columns={columns}
+            />
+          ))}
         </div>
       </div>
     </>
